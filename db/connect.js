494db524b9b10c5a219aeb6a5e3622a18663cafc
Vibhu Garg <vibhugarg507@gmail.com>
2023-10-20 18:48:12 +0530

    Manage Credentials

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = (URL) => {
-  return mongoose.connect("mongodb+srv://vibhugarg507:kalevinu@cluster0.04bmuta.mongodb.net/Jobs"  );
+  return mongoose.connect("mongodb+srv://<username>:<pass>@cluster0.04bmuta.mongodb.net/Jobs"  );
 }
 
 export default connectDB
